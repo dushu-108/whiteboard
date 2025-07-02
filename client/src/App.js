7cc345c11c48ecca86e7cca042ab3294a6ba3933
Y. DUSHYANTH <yalamanchilidushyanth@gmail.com>
2025-07-02 12:34:20 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RoomJoin from './components/RoomJoin';
+import Whiteboard from './components/Whiteboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<RoomJoin />} />
+        <Route path="/room/:roomId" element={<Whiteboard />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
